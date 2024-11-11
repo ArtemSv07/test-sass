2622076ab90787a56855872315045f4994e7d91b
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-11-11 08:56:50 +0000

    Deploying to gh-pages from @ ArtemSv07/test-sass@cdf46b4259dcb35b48a704806670bb2e91534289 🚀

--- assets/index-b0363cf5.js
+++ assets/index-b0363cf5.js
@@ -40,3 +40,12 @@
   }
 })();
 const main = "";
+const button = document.querySelector(".btn-header");
+const menu = document.querySelector(".sidebar");
+const buttonClose = document.querySelector(".js-menu-close");
+button.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+buttonClose.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
